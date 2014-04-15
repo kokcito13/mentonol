7209a59063f5466f2f8b369c4837105ce9c82536
kokcito13 <oklosovich@gmail.com>
2014-04-15 10:26:36 +0300

    filestatus

--- web/app.php
+++ web/app.php
@@ -18,7 +18,7 @@ umask(0000);
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
