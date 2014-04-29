36a116150642bd67776847a5df69f9265be45eb2
kokcito13 <oklosovich@gmail.com>
2014-04-29 15:56:58 +0300

    rubrics

--- application/modules/default/controllers/PageController.php
+++ application/modules/default/controllers/PageController.php
@@ -15,10 +15,6 @@ class PageController extends Zend_Controller_Action
         $this->view->title = $this->view->contentPage['title']->getFieldText();
         $this->view->keywords = $this->view->contentPage['keywords']->getFieldText();
         $this->view->description = $this->view->contentPage['description']->getFieldText();
-
-        $this->view->headText = isset($this->view->contentPage['head'])?$this->view->contentPage['head']->getFieldText():'';
-
-        $this->view->menu = $this->view->page->getRoute()->getUrl();
     }
 
     public function sitemapxmlAction()
@@ -29,8 +25,9 @@ class PageController extends Zend_Controller_Action
 
         $contentPages = Application_Model_Kernel_Page_ContentPage::getList(false, false, false, true, false, 1, false, false, false)->data;
         $categoryList = Application_Model_Kernel_Category::getList(false, false, false, true, false, 1, false, false, false)->data;
+        $posts = Application_Model_Kernel_Post::getList(false, false, false, true, false, 1, false, false, false);
 
-        $pages = array_merge($contentPages, $categoryList);
+        $pages = array_merge($contentPages, $categoryList, $posts);
         foreach ($pages as $page) {
             $container->addPage(Zend_Navigation_Page::factory(array(
                                                                    'uri' => $page->getRoute()->getUrl(),
